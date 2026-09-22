95111b20f3f66bb32febbcd9550602b6c54c00ef
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2026-09-22 22:04:18 +0300

    lesson3

---
+++ lesson5/main.py
@@ -0,0 +1,25 @@
+for x in "banana":
+  print(x)
+print("2second loop")
+
+for y in "banana":
+  print(y)
+  print("Eric")
+
+print("strings lesson")
+
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+print("Eric" in txt)
+
+print("Eric" not in txt)
+
+if "Eric" not in txt :
+    print("Eric txt"  )
+    
+if "Eric" in txt:
+    print("Eric no txt22"  )
