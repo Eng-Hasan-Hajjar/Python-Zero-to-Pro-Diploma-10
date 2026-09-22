6eb10b4de7c5817647f49ff4032758574e7641da
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2026-09-22 22:46:26 +0300

    lesson5

--- lesson5/main.py
+++ lesson5/main.py
@@ -23,3 +23,35 @@ if "Eric" not in txt :
     
 if "Eric" in txt:
     print("Eric no txt22"  )
+
+x=3
+if x==5:
+    print("python")
+    
+if x!=5:
+   print("python5")
+
+
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+   
+   
+b = "Hello, World!"
+print(b[2:5])
+
+laptops="asus"  
+print(laptops) 
+print(laptops[2:3])
+print(laptops[0:3])
+
+
+print(laptops[:3])
+print(laptops[:])
+print(laptops[1:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+print(b[-10:-2])
+print(b[-13:-10])
